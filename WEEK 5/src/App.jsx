33bfde392b3cf94f1cb59cd6af2b9f3916c0e523
Ratna-Manvitha22 <manvitharedie1@gmail.com>
2026-06-10 17:54:51 +0530

    Add files via upload

---
+++ WEEK 5/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'; import Login from './componentss/Login'; 
+import './App.css'; 
+ 
+function App() {   return (     <div> 
+      <Login /> 
+    </div> 
+  ); 
+} 
+ 
+export default App;
